cc5d848c8252c30860543e0447de6d6a3c290e2a
mdilovar <mdilovar@users.noreply.github.com>
2017-06-09 20:39:06 -0500

    Create config.php

--- php/config.php
+++ php/config.php
@@ -1,4 +1,5 @@
 <?php
+    /*
     // DB CREDENTIALS
     define("HOST", "127.0.0.1");
     define("USER", "mdilovar");
@@ -9,4 +10,24 @@
     define("FBAPPSECRET", "803cc531ec2a9d167b0143f32aa76ef4");
     // PASSWORD BLOWFISH HASHING COST - base-2 logarithm itterations
     define("BLOW_COST", 10);
+    */
+/*
+mysql://bf4a2cecb1df1c:2ea0834a@us-cdbr-iron-east-03.cleardb.net/heroku_9e352cfd2b0fd91?reconnect=true
+Everything after the @ symbol until the / is the DB_HOST, us-cdbr-iron-east-02.cleardb.net
+Everything after / until ? is DB_DATABASE, heroku_f7469a0c6b39995
+The string after the // until : is the DB_USERNAME, b419fdf6d21e11
+The string between : and @ is the DB_PASSWORD, c78bdf37
+*/
+    // mysql://bf4a2cecb1df1c:2ea0834a@us-cdbr-iron-east-03.cleardb.net/heroku_9e352cfd2b0fd91?reconnect=true
+    // DB CREDENTIALS
+    define("HOST", "us-cdbr-iron-east-03.cleardb.net");
+    define("USER", "bf4a2cecb1df1c");
+    define("PASSWORD", "2ea0834a");
+    define("DATABASE", "heroku_9e352cfd2b0fd91");
+    // FB APP CREDENTIALS
+    define("FBAPPID", "1500653416897531");
+    define("FBAPPSECRET", "803cc531ec2a9d167b0143f32aa76ef4");
+    // PASSWORD BLOWFISH HASHING COST - base-2 logarithm itterations
+    define("BLOW_COST", 10);
+
 ?>
